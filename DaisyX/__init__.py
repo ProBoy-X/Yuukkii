481ca68b62daf805f019f1d1a0497315ca0ac968
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-05-31 20:02:21 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -28,7 +28,7 @@ from DaisyX.utils.logger import log
 log.info("----------------------")
 log.info("|      Yuki       |")
 log.info("----------------------")
-log.info("Version: " + DAISYX.VERSION) 
+log.info("Version: " + Yuki.VERSION) 
 
 if get_bool_key("DEBUG_MODE") is True:
     DAISY_VERSION += "-debug"
